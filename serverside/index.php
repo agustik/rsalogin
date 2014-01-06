4119cf160ddbfbfe3ac021eb835ff0fc2309f080
agustik <agust.ingi.kjartansson@gmail.com>
2014-01-06 18:27:26 +0000

    more server side

--- serverside/index.php
+++ serverside/index.php
@@ -5,19 +5,32 @@ $password = (array_key_exists('password', $_POST)) ? $_POST['password'] : false;
 $rsa_data = (array_key_exists('rsa_data', $_POST)) ? $_POST['rsa_data'] : false;
 
 
+$private_key = file_get_contents('../keys/private.key');
+
+
 $output = new stdClass();
 
+$decrypted = DecryptRSA($rsa_data, $private_key);
+
+var_dump($decrypted);
+
+
 function DecryptRSA($message,$private_key, $passphrase = false){
+	try {
+		// decode the base 64 message retuned from javascript.
+		$message = base64_decode($message);
+
+		// Unlock the priate key.
+		$key = openssl_get_privatekey($private_key, $passphrase);
+
+		// Decrypt the message.
+		openssl_private_decrypt($encrypted,$newsource,$key);
+
+		// Openssl returns message in base64.. so we need to decode.
+		return base64_decode($newsource);
+	}
+	catch ($e){
+		return false;
+	}
 	
-	// decode the base 64 message retuned from javascript.
-	$message = base64_decode($message);
-
-	// Unlock the priate key.
-	$key = openssl_get_privatekey($private_key, $passphrase);
-
-	// Decrypt the message.
-	openssl_private_decrypt($encrypted,$newsource,$key);
-
-	// Openssl returns message in base64.. so we need to decode.
-	return base64_decode($newsource);
 }
